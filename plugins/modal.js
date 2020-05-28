ff026d22931e0fc299f24a5e5cfe58d1789004ff
artyuhvladislav <workto22@gmail.com>
2020-05-28 19:08:35 +0300

    add: method open() and close()

--- plugins/modal.js
+++ plugins/modal.js
@@ -26,8 +26,12 @@ function _createModal(options) {
 $.modal = function (options) {
     const $modal = _createModal();
     return {
-        open() { },
-        close() { },
+        open() { 
+            $modal.classList.add('open');
+        },
+        close() {
+            $modal.classList.remove('open')
+         },
         destroy() { }
     }
 }
